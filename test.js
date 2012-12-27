c3bb888f3be9733c38d1d3615774b8ebc5d1c1b8
Ben Evans <ben@bensbit.co.uk>
2012-12-27 01:09:13 +0000

    tests

---
+++ test.js
@@ -0,0 +1,16 @@
+
+var assert = require('assert');
+var compact2string = require('./');
+var Buffer = require("buffer").Buffer;
+
+describe('compact2string', function() {
+
+  it('should return expected', function() {
+    assert.equal('10.10.10.5:65408', compact2string(new Buffer("0A0A0A05FF80", "hex")));
+  });
+
+  it('should return expected multi', function() {
+    assert.deepEqual([ '10.10.10.5:128', '100.56.58.99:28525' ], compact2string.multi(new Buffer("0A0A0A05008064383a636f6d", "hex")));
+  });
+
+});
